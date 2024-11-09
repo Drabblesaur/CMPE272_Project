5deb57f5ec9c66e1b5e39ab6b3cb7065a96f5642
Johnny To <tojohnny1559@gmail.com>
2024-11-09 01:50:17 -0800

    Made some basic templates
    used a new component library and put them inside! Pretty cool

---
+++ FrontEnd/db2api/src/app/signup/page.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import { SignupForm } from "@/components/SignupForm";
+
+export default function SignUp() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <SignupForm />
+    </div>
+  );
+}
---
+++ FrontEnd/db2api/src/components/SignupForm.jsx
@@ -0,0 +1,60 @@
+"use client";
+
+import React, { useState } from "react";
+import Link from "next/link";
+
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
+export function SignupForm() {
+  return (
+    <Card className="mx-auto max-w-sm">
+      <CardHeader>
+        <CardTitle className="text-2xl">Sign Up</CardTitle>
+        <CardDescription>
+          Enter your information below to create an account
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <div className="grid gap-4">
+          <div className="grid gap-2">
+            <Label htmlFor="name">Name</Label>
+            <Input id="name" type="text" placeholder="John Doe" required />
+          </div>
+          <div className="grid gap-2">
+            <Label htmlFor="email">Email</Label>
+            <Input id="email" type="email" placeholder="d@email.com" required />
+          </div>
+          <div className="grid gap-2">
+            <Label htmlFor="password">Password</Label>
+            <Input id="password" type="password" required />
+          </div>
+          <div className="grid gap-2">
+            <Label htmlFor="password">Confirm Password</Label>
+            <Input id="password" type="password" required />
+          </div>
+          <Button type="submit" className="w-full">
+            Sign Up
+          </Button>
+          <Button variant="outline" className="w-full">
+            Sign Up with Google
+          </Button>
+        </div>
+        <div className="mt-4 text-center text-sm">
+          Already have an account?{" "}
+          <Link href="/auth" className="underline">
+            Login
+          </Link>
+        </div>
+      </CardContent>
+    </Card>
+  );
+}
